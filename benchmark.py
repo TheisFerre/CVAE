6d9d3b37e2c6286997944fd86adc53956b886461
Theis Ferré Hjortkjær <tfehjo@risoe-r07-g003.cm.cluster>
2019-04-11 11:59:26 +0200

    Added features

---
+++ benchmark.py
@@ -0,0 +1,26 @@
+import vamb
+
+def benchmark(cluster_directory):
+	print('Benchmarking')
+
+	with open("/home/projects/cpr_10006/projects/cnn_vamb/cnn/reference.tsv") as filehandle:
+	    reference = vamb.benchmark.Reference.from_file(filehandle)
+
+	with open(str(cluster_directory)+'/cluster.tsv') as filehandle:
+	    clusters = vamb.cluster.read_clusters(filehandle)
+
+	binning = vamb.benchmark.Binning(clusters, reference)
+	bin_file = open(str(cluster_directory)+'/bins.tsv', 'w')
+	binning.print_matrix(file=bin_file)
+	bin_file.close()
+
+	log_file = open(str(cluster_directory)+'/bins_info.tsv', 'w')
+	print('Bin name', 'DNA in bin', 'number of contigs', sep='\t', file=log_file)
+	for bin_name in binning.bin_precision:
+		print(bin_name, binning.bin_precision[bin_name]['length'], binning.bin_precision[bin_name]['contigs'], sep='\t', file=log_file)
+		#print(bin_name, binning.breadthof[bin_name])
+	return None
+run = input('Write directory: ')
+
+benchmark('/home/projects/cpr_10006/projects/cnn_vamb/cnn/runs/'+run)
+
