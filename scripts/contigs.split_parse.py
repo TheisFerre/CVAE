2bf17b1532bd80efbec477db74a96270d861fb8a
TheisFerre <37203334+TheisFerre@users.noreply.github.com>
2019-05-26 18:02:55 +0200

    Added comments

--- scripts/contigs.split_parse.py
+++ scripts/contigs.split_parse.py
@@ -1,10 +1,11 @@
-##Script for saving splitted contigs of size 2000 into tensors of size [1000, 4, 2000]
+##Script for saving splitted contigs of size 2000 into tensors of size [1000, 4, 2000].
 
 import torch
 import os
 import re
 import numpy as np
 
+##Initialization before creating contig-tensors
 OneHot_dict = {'A':np.array([[1,0,0,0]]), 'T':np.array([[0,1,0,0]]), 'G':np.array([[0,0,1,0]]), 'C':np.array([[0,0,0,1]])}
 OneHot_matrix = np.array([])
 OneHot_tensor = None
@@ -17,6 +18,7 @@ count_num_entries = 0
 count_num_saves = 0
 for line in infile:
 	
+	##Save the contig-tensor if it contains 1000 contig-fragments
 	if count_num_entries % 1000 == 0 and len(OneHot_matrix) > 0:
 		OneHot_matrix = np.array([OneHot_matrix.T])
 		#print(OneHot_matrix.shape)		
@@ -28,6 +30,7 @@ for line in infile:
 		count_num_saves += 1
 		print('Parsed {} entries, {} was saved into file: {}'.format(1000*count_num_saves, OneHot_tensor.shape[0], 'numpy_tensor' +str(count_num_saves)))
 		OneHot_tensor = None
+	##Check if new contig-fragment has desired length
 	if line.startswith('>'):
 		#count_num_entries += 1
 		line_header = line.rstrip()
@@ -37,6 +40,7 @@ for line in infile:
 			count_num_entries += 1
 		read_flag = False
 		DNA_correct = True
+		##Append this DNA to the contig-tensor
 		if len(OneHot_matrix) > 0:
 			OneHot_matrix = np.array([OneHot_matrix.T])
 			if OneHot_tensor is None:
@@ -45,6 +49,7 @@ for line in infile:
 			else:
 				OneHot_tensor = np.concatenate((OneHot_tensor, OneHot_matrix))
 				OneHot_matrix = np.array([])
+	##If fragment length is big enough and DNA is A,T,C og G
 	if read_flag and DNA_length >= 10000 and DNA_correct:
 		for base in line.rstrip():
 			if len(OneHot_matrix) == 0:
@@ -67,6 +72,7 @@ for line in infile:
 					break
 	read_flag = True
 
+##Save last contig-tensor aswell
 if len(OneHot_matrix) > 0:
 	OneHot_matrix = np.array([OneHot_matrix.T])
 	if OneHot_tensor is None:
